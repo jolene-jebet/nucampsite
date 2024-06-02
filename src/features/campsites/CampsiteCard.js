a5448fc9931aa3dbf613d3b0cbef76e36d405a10
jolene <sitienijolene@gmail.com>
2024-06-02 23:08:20 +0300

    update the campsitecard component

--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -1,14 +1,17 @@
 //importing components from reactstrap
 
 import {Card, CardImg, CardImgOverlay, CardTitle} from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 ////defining the component CampsiteCard
 const CampsiteCard = (({ campsite }) => {
     //a const for further destructuring
-    const { image, name } = campsite;
+    const { id, image, name } = campsite;
     //returning statements rendering the things exported from reactstrap
     return(
-        <Card>
+        // this link thing uses tempelate literal syntax to turn the id variable into a string 
+        <Link to={`${id}`}>
+           <Card>
             <CardImg
                 width = '100%'
                 src = {image}
@@ -19,6 +22,8 @@ const CampsiteCard = (({ campsite }) => {
                 <CardTitle>{name}</CardTitle>
             </CardImgOverlay>
         </Card>
+        </Link>
+        
     );
 })
 

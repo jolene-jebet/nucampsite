808ecede8d54db14ad186d709d121b74cf7cf0b4
jolene <sitienijolene@gmail.com>
2024-06-02 22:35:32 +0300

    update the homepage

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -3,7 +3,7 @@ import DisplayCard from './DisplayCard';
 import { selectFeaturedCampsite } from '../campsites/campsitesSlice';
 import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
 
-const DisplayList () => {
+const DisplayList = () => {
     const items = [selectFeaturedCampsite(), selectFeaturedPromotion()];
 
     return (

ba2364eba1df091b61d8677b5880eeae35deb4a0
jolene <sitienijolene@gmail.com>
2024-06-02 23:00:18 +0300

    update the campsiteslist component

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -2,24 +2,20 @@ import { Col, Row, } from 'reactstrap';
 import CampsiteCard from "./CampsiteCard";
 import { selectAllCampsites } from './campsitesSlice';
 
-const CampsitesList = ({ setCampsiteId }) => {
-    const campsites = selectAllCampsites();
+//updating this component so that it no longer takes setCampsiteId function as a prop/ sets an onClick handler to use it
 
-    return (     
-        <Row className="ms-auto">
+const CampsitesList = () => {
+    const campsites = selectAllCampsites();
+    return (
+        <Row className='ms-auto'>
             {campsites.map((campsite) => {
                 return (
-                    <Col 
-                        md="5"
-                        className="m-4" 
-                        key={campsite.id} 
-                        onClick = {() => setCampsiteId(campsite.id)}
-                        >
+                    <Col md='5' className='m-4' key={campsite.id}>
                         <CampsiteCard campsite={campsite} />
                     </Col>
                 );
             })}
-        </Row>  
+        </Row>
     );
 };
 

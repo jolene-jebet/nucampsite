eaec09da436a94d09927d2f9946f21dd2782d45d
jolene <sitienijolene@gmail.com>
2024-06-10 13:35:34 +0300

    add breadcrumbs

--- src/pages/CampsiteDetailPage.js
+++ src/pages/CampsiteDetailPage.js
@@ -2,6 +2,7 @@ import { Container, Row } from 'reactstrap';
 import { useParams } from 'react-router-dom';
 import { selectCampsiteById } from '../features/campsites/campsitesSlice';
 import CampsiteDetail from '../features/campsites/CampsiteDetail';
+import SubHeader from '../components/SubHeader';
 
 function CampsiteDetailPage (){
     //destructuring a variable,campsiteId, from a call to the useParams function
@@ -10,6 +11,8 @@ function CampsiteDetailPage (){
 
     return (
         <Container>
+            {/* the detail prop will cause the second breadcrumb item we set to be conditionally rendered by the subheader component */}
+            <SubHeader current={campsite.name} detail={true} />
             <Row>
                 <CampsiteDetail campsite={campsite} />
             </Row>
